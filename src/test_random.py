78c06dc36fcbe0217dd5322be736491f2d348d76
fedeegiorgi <fgiorgi@mail.utdt.edu>
2024-10-24 18:20:07 -0300

    meson change to compile cython + random test

---
+++ src/test_random.py
@@ -0,0 +1,33 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressorCombiner
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+SEED = 14208
+
+df = pd.read_csv('datasets/salary_football.csv')
+
+# Preprocesamiento de datos
+train_df, validation_df = train_test_split(df, test_size=0.2, random_state=SEED)
+y_train, y_valid = train_df['Wage'], validation_df['Wage']
+X_train, X_valid = train_df.drop('Wage', axis=1), validation_df.drop('Wage', axis=1)
+X_train = pd.get_dummies(X_train)
+X_valid = pd.get_dummies(X_valid)
+X_train, X_valid = X_train.align(X_valid, join='left', axis=1, fill_value=0)
+
+# Inicialización del modelo
+rf = DecisionTreeRegressorCombiner(random_state=SEED)
+rf.fit(X_train, y_train)
+
+trees = rf.estimators_
+samples_used = rf.estimators_samples_
+
+samplesidx = set()
+for samples in samples_used:
+    for sampleidx in samples:
+        samplesidx.add(sampleidx)
+samplesidx = list(samplesidx)
+
+X_union = X_train.iloc[samplesidx].to_numpy()
+y_union = y_train.iloc[samplesidx].to_numpy()
+combinador = DecisionTreeRegressorCombiner(trees, X_union, y_union)
